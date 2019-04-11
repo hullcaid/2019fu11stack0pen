8985a67f8761a30d96c6a58352d68597c4d30ec9
Jussi Leinonen <hullcaid@gmail.com>
2019-04-11 23:19:31 +0300

    Tehtävä 2.6

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -6,29 +6,46 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
-  const handleNoteChange = (event) =>{
+  const handleNameChange = (event) =>{
     console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const addName = (event) => {
     event.preventDefault()
-    setPersons(persons.concat())
+    console.log('Nappi painettu')
+    console.log(newName)
+    const personObject = {
+     name: newName,
+    }
+    console.log(personObject)
+    setPersons(persons.concat(personObject))
+    setNewName('')
   }
+
+  const Listing = ({person}) => <li>{person.name}</li>
+
+  const rows = () => persons.map(person =>
+    <Listing 
+      key={person.name}
+      person={person}
+      />
+    )
 
   return (
     <div>
       <h2>Puhelinluettelo</h2>
       <form onSubmit={addName}>
         <div>
-          nimi: <input value={newName} onChange={handleNoteChange}/>
+          nimi: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">lisää</button>
         </div>
       </form>
       <h2>Numerot</h2>
-      ...
+      {rows()}
+      <div>debug: {newName}</div>
     </div>
   )
 
