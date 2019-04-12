623758fc145a4de92ddbde2d9df1fa0da8dcd52f
Jussi Leinonen <hullcaid@gmail.com>
2019-04-12 12:56:29 +0300

    Tehtävä 2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,14 +1,30 @@
 import React, { useState } from 'react'
 
+
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Martti Tienari', number: '040-123456' },
+    { name: 'Arto Järvinen', number: '040-123456' },
+    { name: 'Lea Kutvonen', number: '040-123456' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setNewFilter ] = useState('')
 
   const handleNameChange = (event) =>{
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    //console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setNewFilter(event.target.value)
   }
 
   const addName = (event) => {
@@ -27,35 +43,43 @@ const App = () => {
     if(!exists) {
       const personObject = {
         name: newName,
+        number: newNumber,
       }
       
       console.log(personObject)
       
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
-  const Listing = ({person}) => <li>{person.name}</li>
+  const Listing = ({person}) => <li>{person.name} {person.number}</li>
 
-  const rows = () => persons.map(person =>
-    <Listing 
-      key={person.name}
-      person={person}
-      />
-    )
+  const filteredList = filter.length >0 ? persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()) ) : persons
+
+  const rows = () => filteredList.map(person =>
+  <Listing key={person.name} person={person} />) 
+    
 
   return (
     <div>
-      <h2>Puhelinluettelo</h2>
-      <form onSubmit={addName}>
+      <h1>Puhelinluettelo</h1>
         <div>
-          nimi: <input value={newName} onChange={handleNameChange}/>
+          rajaa näytettäviä: <input value={filter} onChange={handleFilterChange}/>
         </div>
-        <div>
-          <button type="submit">lisää</button>
-        </div>
-      </form>
+      <h2>Lisää uusi</h2>
+        <form onSubmit={addName}>
+          <div>
+            nimi: <input value={newName} onChange={handleNameChange}/>
+          </div>
+          <div>
+            numero: <input value={newNumber} onChange={handleNumberChange}/>
+          </div>
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
       <h2>Numerot</h2>
       {rows()}
     </div>
