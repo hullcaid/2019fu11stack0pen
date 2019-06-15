d657043e0734d12c5dced7bb5577e2a14702278d
Jussi Leinonen <hullcaid@gmail.com>
2019-06-15 17:27:44 +0300

    Tehtävä 4.2 - Valmis

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,24 +1,11 @@
-require('dotenv').config();
+const app = require('./app');
 const http = require('http');
 const config = require('./utils/config');
 
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const blogsRouter = require('./controllers/blogs');
+const server = http.createServer(app);
 
-
-const mongoUrl = config.MONGODB_URI;
-mongoose.connect(mongoUrl, { useNewUrlParser: true });
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use('/api/blogs', blogsRouter);
-
-
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
 	console.log(`Server running on port ${config.PORT}`);
 });
+
+
