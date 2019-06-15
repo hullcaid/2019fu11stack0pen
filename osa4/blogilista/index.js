e6b62f941028500486a6253a8e2fa7d2249069c0
Jussi Leinonen <hullcaid@gmail.com>
2019-06-15 16:47:53 +0300

    Tehtävä 4.2 ympäristömuuttujat omassa moduulissa

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const config = require('./utils/config')
 const http = require('http');
 const express = require('express');
 const app = express();
@@ -16,7 +17,7 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema);
 
-const mongoUrl = process.env.MONGODB_URI;
+const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl, { useNewUrlParser: true });
 
 app.use(cors());
@@ -40,7 +41,6 @@ app.post('/api/blogs', (request, response) => {
 		});
 });
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+	console.log(`Server running on port ${config.PORT}`);
 });
--- osa4/blogilista/utils/config.js
+++ osa4/blogilista/utils/config.js
@@ -1,4 +1,9 @@
 require('dotenv').config();
 
-let PORT = 3003
-let MONGO
+let PORT = process.env.PORT;
+let MONGODB_URI = process.env.MONGODB_URI;
+
+module.exports = {
+	PORT,
+	MONGODB_URI
+};
