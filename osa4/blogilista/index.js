6ca0b3c901a6ab2ec56a98b9d095ac4ceabf4f27
Jussi Leinonen <hullcaid@gmail.com>
2019-06-15 17:16:38 +0300

    Tehtävä 4.2 - Router

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -7,31 +7,17 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const blogsRouter = require('./controllers/blogs');
+
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl, { useNewUrlParser: true });
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use('/api/blogs', blogsRouter);
 
-app.get('/api/blogs', (request, response) => {
-	Blog
-		.find({})
-		.then(blogs => {
-			response.json(blogs);
-		});
-});
 
-app.post('/api/blogs', (request, response) => {
-	const blog = new Blog(request.body);
-
-	blog
-		.save()
-		.then(result => {
-			response.status(201).json(result);
-		});
-});
 
 app.listen(config.PORT, () => {
 	console.log(`Server running on port ${config.PORT}`);
