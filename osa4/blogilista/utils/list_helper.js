ea4189759cbf27cff7e71b5fb2c2245ae9df52f6
Jussi Leinonen <hullcaid@gmail.com>
2019-06-16 15:06:35 +0300

    Tehtävä 4.4.

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,8 +1,31 @@
 const dummy = (blogs) => {
-	console.log(blogs);
 	return(1);
 };
 
+/* const totalLikes = (blogs) => {
+	//console.log(`received: ${blogs}`);
+	const reducer = (sum, item) => {
+		//console.log(`reduserin syöte: ${sum}, ${typeof(sum)}, ${item}, ${typeof(item)}` );
+		const value = sum + item;
+		//console.log(`välitulos: ${value}, ${typeof(value)}`);
+		return value;
+	};
+
+	const total = blogs.length === 0
+		? 0
+		: blogs.reduce((sum, blog) => reducer(sum, blog.likes), 0);
+	//console.log(`sum of likes: ${total}`);
+	return total;
+}; */
+
+const totalLikes = (blogs) => {
+	const total = blogs.length === 0
+		? 0
+		: blogs.reduce((sum, blog) => sum + blog.likes, 0);
+	return total;
+};
+
+
 module.exports = {
-	dummy
+	dummy, totalLikes
 };
